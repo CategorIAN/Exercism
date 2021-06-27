801e8b4f93916449409f1c3c5ae7ee7c94fd2805
Ian H. Kessler <ianholm9@gmail.com>
2021-06-27 13:11:10 -0600

    Finished matrix.

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,19 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        M = []
+        for i in matrix_string.split("\n"):
+            r = []
+            for j in i.split(" "):
+                r.append(int(j))
+            M.append(r)
+        self.M = M
 
     def row(self, index):
-        pass
+        return self.M[index-1]
 
     def column(self, index):
-        pass
+        C = []
+        for r in self.M:
+            C.append(r[index-1])
+        return C
+
